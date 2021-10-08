62c97fe9eed064af96a055933126661e54f85e98
auta1 <92181394+auta1@users.noreply.github.com>
2021-10-08 23:34:32 +0300

    Add files via upload
    only change the settings for secrete key

---
+++ myproject/urls.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+
+from boards import views
+from accounts import views as account_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('markdown/', include('django_markdown.urls')),
+    path("", views.ForumListView.as_view(), name='home'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path("boards/<pk>/", views.board_topics, name='board_topics'),
+    path("boards/<pk>/new/", views.new_topic, name='new_topic'),
+
+    path('signup/', account_views.signup, name='signup'),
+    path('reset/', auth_views.PasswordResetView.as_view(
+        template_name='password_reset.html',
+        email_template_name='password_reset_email.html',
+        subject_template_name='password_reset_subject.txt',
+    ), name='password_reset'),
+    path('reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
+         name="password_reset_confirm"),
+    path('reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+         name='password_reset_complete'),
+    path('settings/password/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+         name='password_change'),
+    path('settings/password/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+         name='password_change_done'),
+    path('boards/<pk>/topics/<topic_pk>/',
+         views.PostListView.as_view(), name='topic_posts'),
+    path('boards/<pk>/topics/<topic_pk>/reply/', views.reply_topic, name='reply_topic'),
+    path('boards/<pk>/topics/<topic_pk>/post/<post_pk>/edit/',
+         views.PostUpdateView.as_view(), name="edit_post"),
+    path('setting/accounts/', account_views.UserUpdateView.as_view(), name='user_account'),
+]
